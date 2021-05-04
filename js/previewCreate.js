2be2691d3ed4f027bfd583b2dc9312c2ceeaf8dc
2ffler <83592582+2ffler@users.noreply.github.com>
2021-05-04 15:03:03 +0900

    Create previewCreate.js

---
+++ js/previewCreate.js
@@ -0,0 +1,37 @@
+var MyBG;
+if(window.File) {
+	var backPreview = document.getElementById('backPreview');
+	var select = document.querySelector('input[name="display_back"]');
+	// ファイルが選択されたとき
+	select.addEventListener('change', function(e) {
+		// 選択されたファイルの情報を取得
+		const fileData = e.target.files[0];
+
+		const imgType = fileData.type;
+		// 選択されたファイルが画像かどうか確認
+		if(!imgType.match(/^image/)) {
+			alert('画像ファイルじゃないんじゃないそれ。');
+			select.value = '';
+			backPreview.innerHTML = '画像だったらここにプレビュー出てくるんだけど。';
+			return;
+		}
+		var reader = new FileReader();
+		// ファイル読み取りに失敗したとき
+		reader.onerror = function() {
+			alert('読み取り失敗！')
+			backPreview.innerHTML = '';
+		}
+		// ファイル読み取りに成功したとき
+		reader.onload = function() {
+			const insert = '※推奨画像比率・縦長９：16<h4>背景画像プレビュー↓</h4><div class="frame"><img src="' + reader.result + '" class="backPreview"></div>';
+			const buttonSet ='<button type="submit" name="fileCreate" onclick="buttonClick(this)">確認</button>';
+			backPreview.innerHTML = insert;
+			buttonThere.innerHTML = buttonSet;
+		}
+		// ファイル読み取りを実行
+		reader.readAsDataURL(fileData);
+		MyBG = 1;
+
+	}, false);
+		
+};
